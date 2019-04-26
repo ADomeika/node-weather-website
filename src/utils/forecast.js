87d86a2b17e7930d230444ffca3ebbb0923c3917
Adomas Domeika <adomas.domeika@jet2.com>
2019-04-26 11:49:38 +0100

    Changed forecast text

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -10,7 +10,11 @@ exports.forecast = (latitude, longitude, callback) => {
     } else {
       const { currently } = body
       callback(undefined, {
-        summary: `${body.daily.data[0].summary} It is currently ${currently.temperature} degrees out. There is a ${currently.precipProbability}% chance of rain.`,
+        summary: `${body.daily.data[0].summary}
+          It is currently ${currently.temperature} degrees out.
+          The high today is ${body.daily.data[0].temperatureHigh}.
+          The low today is ${body.daily.data[0].temperatureLow}.
+          There is a ${currently.precipProbability}% chance of rain.`,
         temperature: currently.temperature,
         rainProbability: currently.precipProbability
       })
